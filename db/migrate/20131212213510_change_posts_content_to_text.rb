0df6f7ae212dafca6bbdce816e0adb076b3f665b
Eric Kelly <heroiceric@gmail.com>
2013-12-20 19:26:55 -0500

    Remove random lecture stuff in migration

--- db/migrate/20131212213510_change_posts_content_to_text.rb
+++ db/migrate/20131212213510_change_posts_content_to_text.rb
@@ -1,14 +1,9 @@
 class ChangePostsContentToText < ActiveRecord::Migration
   def up
-    Post.where(published_at: nil).each do |post|
-      post.published_at = Time.now
-    end
-
     change_column(:posts, :content, :text) 
-    change_column(:posts, :published_at, :datetime, null: false) 
   end
 
   def down
-    change_column(:posts, :content, :string) 
+    change_column(:posts, :content, :string)
   end
 end
