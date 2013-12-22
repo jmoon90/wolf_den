5412d7966eb1e940f48f98906fe38ab0eaa2bd22
John Moon <johnmoonyy@gmail.com>
2013-12-22 00:32:15 -0500

    Fix sign up tests to pass

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,8 @@
 
 FactoryGirl.define do
   factory :user do
+    email "john@foobar.com"
+    password 'foobar01'
+    password_confirmation 'foobar01'
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,26 +1,23 @@
 require 'spec_helper'
 
 describe User do
-  describe 'sign up' do
-    it 'creates new user with valid input' do
-      visit '/sessions/new'
-      within('#session') do
-        fill_in 'name', with: 'john'
-        fill_in 'email', with: 'john@aol.com'
-        fill_in 'password', with: 'foobar'
-        fill_in 'password_confirmation', with: 'foobar'
-      end
+  describe 'Sign up', :type => :feature do
+    let(:submit) { "Sign up" }
 
-      click_link 'sign up'
-      expect(page).to have_content 'success'
+    let(:person) do
+      @user = FactoryGirl.create(:user)
     end
 
-    it 'does not creat new user with invalid input' do
-      visit '/sessions/new'
-        fill_in 'name', with: ''
-        fill_in 'email', with: 'alskdjf'
-        fill_in 'password', with: ''
-        fill_in 'password_confirmation', with: ''
+    it 'with valid information' do
+      visit new_user_registration_path
+      user_count = User.count
+      person
+      expect(User.count).to eq(user_count + 1)
+    end
+
+    it 'without valid information' do
+      visit new_user_registration_path
+      expect{ click_button submit }.not_to change(User, :count)
     end
   end
 end
