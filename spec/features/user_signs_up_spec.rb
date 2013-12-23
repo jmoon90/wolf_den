1837736f527881420e5997360a600f51f6923021
John Moon <johnmoonyy@gmail.com>
2013-12-23 12:56:31 -0500

    Test error message with uniqueness

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 feature 'Sign up' do
-  let(:submit) { "Sign up" }
 
   scenario 'signs people up with valid information' do
     visit new_user_registration_path
@@ -14,5 +13,28 @@ feature 'Sign up' do
     end
 
     expect(page).to have_content 'Welcome! You have signed up successfully.'
+  end
+
+  scenario 'error message with invalid or email exists' do
+    visit new_user_registration_path
+
+    within('form#new_user') do
+      fill_in 'Email', with: 'jmoon90@aol.com'
+      fill_in "Password", with: 'cherrypie'
+      fill_in "Password confirmation", with: 'cherrypie'
+      click_on 'Sign up'
+    end
+
+    visit destroy_user_session_path
+    visit new_user_registration_path
+
+    within('form#new_user') do
+      fill_in 'Email', with: 'jmoon90@aol.com'
+      fill_in "Password", with: 'cherrypie'
+      fill_in "Password confirmation", with: 'cherrypie'
+      click_on 'Sign up'
+    end
+
+    expect(page).to have_content 'Email has already been taken'
   end
 end
